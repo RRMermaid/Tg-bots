851ee64490266c2e6b598af17128c454a0cb2a8d
RRMermaid <rnizametdinova1991@gmail.com>
2025-09-05 05:06:08 +0500

    + if not api_key

--- Nalegke-bot.py
+++ Nalegke-bot.py
@@ -5,6 +5,9 @@ load_dotenv()  # Загружает переменные окружения из
 
 api_key = os.getenv("OPENAI_API_KEY")
 
+if not api_key:
+    raise RuntimeError("OPENAI_API_KEY отсутствует в окружении")
+
 from openai import OpenAI
 client = OpenAI(api_key=api_key)
 from telegram import Update
